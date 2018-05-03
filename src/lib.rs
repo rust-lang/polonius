1761afe52b880fd3aff07f5e9ae36dac688b7941
Niko Matsakis <niko@alum.mit.edu>
2018-05-03 06:42:23 -0400

    adopt extern-prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,13 @@
 #![feature(catch_expr)]
 #![feature(crate_in_paths)]
 #![feature(crate_visibility_modifier)]
+#![feature(extern_absolute_paths)]
+#![feature(extern_prelude)]
 #![feature(proc_macro)]
 #![feature(in_band_lifetimes)]
 #![feature(termination_trait_test)]
 
 #![allow(dead_code)]
-
-extern crate abomonation;
-extern crate abomonation_derive;
-extern crate differential_dataflow;
-extern crate failure;
-extern crate fxhash;
-extern crate timely;
-extern crate structopt;
-
-#[macro_use]
-extern crate clap;
 
 mod facts;
 mod intern;
