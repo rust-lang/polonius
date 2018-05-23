bb86d72dc2c66276ce41c82292be109260dc524d
Rémy Rakic <remy.rakic@gmail.com>
2018-05-23 20:57:37 +0200

    Implement friendly front-end format
    Implement a friendlier front-end format destined to help create
    isolated tests, without needing rustc NLL facts, but still generating
    the same data structure.

---
+++ polonius-parser/src/tests.rs
@@ -0,0 +1,198 @@
+#![cfg(test)]
+
+use crate::ir::Effect;
+use crate::parse_input;
+
+#[test]
+fn universal_regions() {
+    let program = r"
+        universal_regions { 'a, 'b, 'c }
+    ";
+    let input = parse_input(program);
+    assert!(input.is_ok());
+
+    let input = input.unwrap();
+    assert_eq!(input.universal_regions, ["'a", "'b", "'c"]);
+}
+
+#[test]
+fn blocks() {
+    let program = r"
+        universal_regions { 'a, 'b, 'c }
+        block B0 {
+        }
+        block B1 {
+        }
+    ";
+    let input = parse_input(program);
+    assert!(input.is_ok());
+
+    let input = input.unwrap();
+    assert_eq!(
+        input.blocks.iter().map(|b| &b.name).collect::<Vec<_>>(),
+        ["B0", "B1"]
+    );
+}
+
+#[test]
+fn goto() {
+    let program = r"
+        universal_regions { 'a, 'b, 'c }
+        block B0 {
+            goto B1;
+        }
+        block B1 {
+        }
+    ";
+    let input = parse_input(program);
+    assert!(input.is_ok());
+
+    let input = input.unwrap();
+    assert_eq!(input.blocks[0].goto, ["B1"]);
+}
+
+#[test]
+fn effects() {
+    let program = r"
+        universal_regions { 'a, 'b, 'c }
+        block B0 {
+            use('a), outlives('a: 'b), borrow_region_at('b, L1);
+            kill(L2);
+            invalidates(L0);
+        }
+    ";
+    let input = parse_input(program);
+    assert!(input.is_ok());
+
+    let input = input.unwrap();
+    let block = &input.blocks[0];
+    assert_eq!(block.statements.len(), 3);
+
+    let statements = &block.statements;
+    assert_eq!(statements[0].effects.len(), 3);
+    assert_eq!(statements[1].effects.len(), 1);
+    assert_eq!(statements[2].effects.len(), 1);
+
+    let effects = &statements[0].effects;
+    assert_eq!(
+        effects[0],
+        Effect::Use {
+            region: "'a".to_string()
+        }
+    );
+    assert_eq!(
+        effects[1],
+        Effect::Outlives {
+            a: "'a".to_string(),
+            b: "'b".to_string()
+        }
+    );
+    assert_eq!(
+        effects[2],
+        Effect::BorrowRegionAt {
+            region: "'b".to_string(),
+            loan: "L1".to_string()
+        }
+    );
+
+    let effects = &statements[1].effects;
+    assert_eq!(
+        effects[0],
+        Effect::Kill {
+            loan: "L2".to_string()
+        }
+    );
+
+    let effects = &statements[2].effects;
+    assert_eq!(
+        effects[0],
+        Effect::Invalidates {
+            loan: "L0".to_string()
+        }
+    );
+}
+
+#[test]
+fn effects_start() {
+    let program = r"
+        universal_regions { 'a, 'b, 'c }
+        block B0 {
+            invalidates(L0) / use('a);
+            invalidates(L1);
+            invalidates(L0), invalidates(L1) / use('c);
+        }
+    ";
+    let input = parse_input(program);
+    assert!(input.is_ok());
+
+    let input = input.unwrap();
+    let block = &input.blocks[0];
+    assert_eq!(block.statements.len(), 3);
+
+    let statements = &block.statements[0];
+    assert_eq!(
+        statements.effects_start,
+        [Effect::Invalidates {
+            loan: "L0".to_string()
+        }]
+    );
+    assert_eq!(
+        statements.effects,
+        [Effect::Use {
+            region: "'a".to_string()
+        }]
+    );
+
+    let statements = &block.statements[1];
+    assert!(statements.effects_start.is_empty());
+    assert_eq!(
+        statements.effects,
+        [Effect::Invalidates {
+            loan: "L1".to_string()
+        }]
+    );
+
+    let statements = &block.statements[2];
+    assert_eq!(
+        statements.effects_start,
+        [
+            Effect::Invalidates {
+                loan: "L0".to_string()
+            },
+            Effect::Invalidates {
+                loan: "L1".to_string()
+            }
+        ]
+    );
+    assert_eq!(
+        statements.effects,
+        [Effect::Use {
+            region: "'c".to_string()
+        }]
+    );
+}
+
+#[test]
+fn complete_example() {
+    let program = r"
+        // program description
+        universal_regions { 'a, 'b, 'c }
+
+        // block description
+        block B0 {
+            // 0:
+            invalidates(L0);
+
+            // 1:
+            kill(L2);
+
+            // another comment
+            goto B1, B2;
+        }
+
+        block B1 {
+            use('a), outlives('a: 'b), borrow_region_at('b, L1);
+        }
+    ";
+    assert!(parse_input(program).is_ok());
+}
---
+++ src/program.rs
@@ -0,0 +1,315 @@
+use std::collections::BTreeSet;
+
+use polonius_parser::{ir::Effect, parse_input};
+
+use crate::facts::{AllFacts, Loan, Point, Region};
+use crate::intern::InternerTables;
+
+/// A structure to hold and deduplicate facts
+#[derive(Default)]
+struct Facts {
+    borrow_region: BTreeSet<(Region, Loan, Point)>,
+    universal_region: BTreeSet<Region>,
+    cfg_edge: BTreeSet<(Point, Point)>,
+    killed: BTreeSet<(Loan, Point)>,
+    outlives: BTreeSet<(Region, Region, Point)>,
+    region_live_at: BTreeSet<(Region, Point)>,
+    invalidates: BTreeSet<(Point, Loan)>,
+}
+
+impl From<Facts> for AllFacts {
+    fn from(facts: Facts) -> Self {
+        Self {
+            borrow_region: facts.borrow_region.into_iter().collect(),
+            universal_region: facts.universal_region.into_iter().collect(),
+            cfg_edge: facts.cfg_edge.into_iter().collect(),
+            killed: facts.killed.into_iter().collect(),
+            outlives: facts.outlives.into_iter().collect(),
+            region_live_at: facts.region_live_at.into_iter().collect(),
+            invalidates: facts.invalidates.into_iter().collect(),
+        }
+    }
+}
+
+/// Parses an input program into a set of its facts, into the same format `rustc` outputs.
+crate fn parse_from_program(
+    program: &str,
+    tables: &mut InternerTables,
+) -> Result<AllFacts, String> {
+    let input = parse_input(program)?;
+
+    let mut facts: Facts = Default::default();
+
+    // facts: universal_region(Region)
+    for region in &input.universal_regions {
+        let region = tables.regions.intern(region);
+        facts.universal_region.insert(region);
+    }
+
+    for block in &input.blocks {
+        let block_name = &block.name;
+
+        for (statement_idx, statement) in block.statements.iter().enumerate() {
+            let start = format!(
+                "\"Start({block}[{statement}])\"",
+                block = block_name,
+                statement = statement_idx
+            );
+            let mid = format!(
+                "\"Mid({block}[{statement}])\"",
+                block = block_name,
+                statement = statement_idx
+            );
+
+            let start = tables.points.intern(&start);
+            let mid = tables.points.intern(&mid);
+
+            // facts: cfg_edge(Point, Point)
+            {
+                if statement_idx > 0 {
+                    // edge: Previous Mid point to this Start point
+                    let previous_mid = format!(
+                        "\"Mid({block}[{previous_statement}])\"",
+                        block = block_name,
+                        previous_statement = statement_idx - 1
+                    );
+                    let previous_mid = tables.points.intern(&previous_mid);
+
+                    facts.cfg_edge.insert((previous_mid, start));
+                }
+
+                // edge: Start to Mid point
+                facts.cfg_edge.insert((start, mid));
+
+                // goto edges
+                let terminator_idx = block.statements.len() - 1;
+                for goto in &block.goto {
+                    // edge: last Mid point to Start of remote block
+                    let from = format!(
+                        "\"Mid({block}[{statement}])\"",
+                        block = block_name,
+                        statement = terminator_idx
+                    );
+                    let to = format!("\"Start({next_block}[0])\"", next_block = goto);
+
+                    let from = tables.points.intern(&from);
+                    let to = tables.points.intern(&to);
+
+                    facts.cfg_edge.insert((from, to));
+                }
+            }
+
+            // the most common statement effects: mid point effects
+            for effect in &statement.effects {
+                match effect {
+                    // facts: region_live_at(Region, Point)
+                    Effect::Use { ref region } => {
+                        // region_live_at: a `use` (in Mid points) emits a `region_live_at` fact at Start points
+                        let region = tables.regions.intern(region);
+                        facts.region_live_at.insert((region, start));
+                    }
+
+                    _ => emit_effect(&mut facts, effect, mid, tables),
+                };
+            }
+
+            // commonly reserved for
+            for effect in &statement.effects_start {
+                emit_effect(&mut facts, effect, start, tables);
+            }
+        }
+    }
+
+    Ok(facts.into())
+}
+
+fn emit_effect(facts: &mut Facts, effect: &Effect, point: Point, tables: &mut InternerTables) {
+    match effect {
+        // facts: borrow_region(Region, Loan, Point)
+        Effect::BorrowRegionAt {
+            ref region,
+            ref loan,
+        } => {
+            // borrow_region: a `borrow_region_at` occurs on the Mid point
+            let region = tables.regions.intern(region);
+            let loan = tables.loans.intern(loan);
+
+            facts.borrow_region.insert((region, loan, point));
+        }
+
+        // facts: outlives(Region, Region, Point)
+        Effect::Outlives { ref a, ref b } => {
+            // outlives: a `outlives` occurs on Mid points
+            let region_a = tables.regions.intern(a);
+            let region_b = tables.regions.intern(b);
+
+            facts.outlives.insert((region_a, region_b, point));
+        }
+
+        // facts: killed(Loan, Point)
+        Effect::Kill { ref loan } => {
+            // killed: a loan is killed on Mid points
+            let loan = tables.loans.intern(loan);
+            facts.killed.insert((loan, point));
+        }
+
+        // facts: invalidates(Point, Loan)
+        Effect::Invalidates { ref loan } => {
+            let loan = tables.loans.intern(loan);
+            // invalides: a loan can be invalidated on both Start and Mid points
+            facts.invalidates.insert((point, loan));
+        }
+
+        _ => panic!("Unexpected effect: {:?}", effect),
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::intern::InternerTables;
+
+    #[test]
+    fn complete_program() {
+        let program = r"
+            // program description
+            universal_regions { 'a, 'b, 'c }
+
+            // block description
+            block B0 {
+                // 0:
+                invalidates(L0);
+
+                // 1:
+                invalidates(L1) / kill(L2);
+
+                // another comment
+                goto B1;
+            }
+
+            block B1 {
+                // O:
+                use('a), outlives('a: 'b), borrow_region_at('b, L1);
+            }
+        ";
+
+        let mut tables = InternerTables::new();
+
+        let facts = parse_from_program(program, &mut tables);
+        assert!(facts.is_ok());
+
+        let facts = facts.unwrap();
+
+        // facts: universal_region
+        let universal_regions: Vec<_> = facts
+            .universal_region
+            .iter()
+            .map(|r| tables.regions.untern(*r).to_string())
+            .collect();
+        assert_eq!(universal_regions, ["'a", "'b", "'c"]);
+
+        // facts: invalidates
+        assert_eq!(facts.invalidates.len(), 2);
+        {
+            // regular mid point `invalidates`
+            let point = tables.points.untern(facts.invalidates[0].0);
+            let loan = tables.loans.untern(facts.invalidates[0].1);
+
+            assert_eq!(point, "\"Mid(B0[0])\"");
+            assert_eq!(loan, "L0");
+        }
+
+        {
+            // uncommon start point `invalidates`
+            let point = tables.points.untern(facts.invalidates[1].0);
+            let loan = tables.loans.untern(facts.invalidates[1].1);
+
+            assert_eq!(point, "\"Start(B0[1])\"");
+            assert_eq!(loan, "L1");
+        }
+
+        // facts: region_live_at
+        assert_eq!(facts.region_live_at.len(), 1);
+        {
+            let region = tables.regions.untern(facts.region_live_at[0].0);
+            let point = tables.points.untern(facts.region_live_at[0].1);
+
+            assert_eq!(region, "'a");
+            assert_eq!(point, "\"Start(B1[0])\"");
+        }
+
+        assert_eq!(facts.outlives.len(), 1);
+        {
+            let region_a = tables.regions.untern(facts.outlives[0].0);
+            let region_b = tables.regions.untern(facts.outlives[0].1);
+            let point = tables.points.untern(facts.outlives[0].2);
+
+            assert_eq!(region_a, "'a");
+            assert_eq!(region_b, "'b");
+            assert_eq!(point, "\"Mid(B1[0])\"");
+        }
+
+        assert_eq!(facts.borrow_region.len(), 1);
+        {
+            let region = tables.regions.untern(facts.borrow_region[0].0);
+            let loan = tables.loans.untern(facts.borrow_region[0].1);
+            let point = tables.points.untern(facts.borrow_region[0].2);
+
+            assert_eq!(region, "'b");
+            assert_eq!(loan, "L1");
+            assert_eq!(point, "\"Mid(B1[0])\"");
+        }
+
+        assert_eq!(facts.killed.len(), 1);
+        {
+            let loan = tables.loans.untern(facts.killed[0].0);
+            let point = tables.points.untern(facts.killed[0].1);
+
+            assert_eq!(loan, "L2");
+            assert_eq!(point, "\"Mid(B0[1])\"");
+        }
+
+        // 6 points (3 statements * 2 points) => 5 edges, including the 1 goto edge
+        let points: BTreeSet<Point> = facts
+            .cfg_edge
+            .iter()
+            .map(|&(p, _)| p)
+            .chain(facts.cfg_edge.iter().map(|&(_, q)| q))
+            .collect();
+        assert_eq!(points.len(), 6);
+        assert_eq!(facts.cfg_edge.len(), 5);
+
+        let mut make_edge = |a, b| (tables.points.intern(a), tables.points.intern(b));
+
+        // Start to Mid edge, per statement
+        assert!(
+            facts
+                .cfg_edge
+                .contains(&make_edge("\"Start(B0[0])\"", "\"Mid(B0[0])\""))
+        );
+        assert!(
+            facts
+                .cfg_edge
+                .contains(&make_edge("\"Start(B0[1])\"", "\"Mid(B0[1])\""))
+        );
+        assert!(
+            facts
+                .cfg_edge
+                .contains(&make_edge("\"Start(B1[0])\"", "\"Mid(B1[0])\""))
+        );
+
+        // Mid to Start edge, per statement pair for each block
+        assert!(
+            facts
+                .cfg_edge
+                .contains(&make_edge("\"Mid(B0[0])\"", "\"Start(B0[1])\""))
+        );
+
+        // 1 goto edge from B0 to B1
+        assert!(
+            facts
+                .cfg_edge
+                .contains(&make_edge("\"Mid(B0[1])\"", "\"Start(B1[0])\""))
+        );
+    }
+}
