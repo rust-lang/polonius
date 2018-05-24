c8c506b6842d132995a85a576d6fdda58bc94edf
Santiago Pastorino <spastorino@gmail.com>
2018-05-24 18:43:02 -0300

    Implement default for AllFacts

--- polonius-engine/src/facts.rs
+++ polonius-engine/src/facts.rs
@@ -1,7 +1,7 @@
 use std::hash::Hash;
 
 /// The "facts" which are the basis of the NLL borrow analysis.
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct AllFacts<R: Atom, L: Atom, P: Atom> {
     /// `borrow_region(R, B, P)` -- the region R may refer to data
     /// from borrow B starting at the point P (this is usually the
@@ -25,6 +25,20 @@ pub struct AllFacts<R: Atom, L: Atom, P: Atom> {
 
     ///  `invalidates(P, L)` when the loan L is invalidated at point P
     pub invalidates: Vec<(P, L)>
+}
+
+impl<R: Atom, L: Atom, P: Atom> Default for AllFacts<R, L, P> {
+    fn default() -> Self {
+        AllFacts {
+            borrow_region: Vec::default(),
+            universal_region: Vec::default(),
+            cfg_edge: Vec::default(),
+            killed: Vec::default(),
+            outlives: Vec::default(),
+            region_live_at: Vec::default(),
+            invalidates: Vec::default(),
+        }
+    }
 }
 
 pub trait Atom: From<usize> + Into<usize> + Copy + Clone + Eq + Ord + Hash + 'static {
