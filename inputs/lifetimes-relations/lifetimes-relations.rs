94f7c2a3b21d52609c6b5f6f8ea770abda2daf33
lqd <remy.rakic+github@gmail.com>
2018-12-21 18:14:28 +0100

    Add an `implied_bound_subset` test
    Start gathering more samples for the different lifetime relations we can have

--- inputs/lifetimes-relations/lifetimes-relations.rs
+++ inputs/lifetimes-relations/lifetimes-relations.rs
@@ -5,6 +5,10 @@ fn missing_subset<'a, 'b>(x: &'a u32, y: &'b u32) -> &'a u32 {
     y //~ ERROR
 }
 
+fn implied_bound_subset<'a, 'b>(x: &'b &'a mut u32) -> &'b u32 {
+    x
+}
+
 fn valid_subset<'a, 'b: 'a>(x: &'a u32, y: &'b u32) -> &'a u32 {
     y
 }
