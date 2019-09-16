f58fff2a40174b5731cf3105c756086d3f60d255
Rémy Rakic <remy.rakic@gmail.com>
2019-09-16 22:15:55 +0200

    Rename `Region` to `Origin` in `AllFacts`, and use it as a word
    Just like everywhere else

--- polonius-engine/src/facts.rs
+++ polonius-engine/src/facts.rs
@@ -3,14 +3,14 @@ use std::hash::Hash;
 
 /// The "facts" which are the basis of the NLL borrow analysis.
 #[derive(Clone, Debug)]
-pub struct AllFacts<R: Atom, L: Atom, P: Atom, V: Atom, M: Atom> {
-    /// `borrow_region(R, B, P)` -- the region R may refer to data
+pub struct AllFacts<Origin: Atom, L: Atom, P: Atom, V: Atom, M: Atom> {
+    /// `borrow_region(O, B, P)` -- the origin `O` may refer to data
     /// from borrow B starting at the point P (this is usually the
     /// point *after* a borrow rvalue)
-    pub borrow_region: Vec<(R, L, P)>,
+    pub borrow_region: Vec<(Origin, L, P)>,
 
-    /// `universal_region(R)` -- this is a "free region" within fn body
-    pub universal_region: Vec<R>,
+    /// `universal_region(O)` -- this is a "free region" within fn body
+    pub universal_region: Vec<Origin>,
 
     /// `cfg_edge(P,Q)` for each edge P -> Q in the control flow
     pub cfg_edge: Vec<(P, P)>,
@@ -18,8 +18,8 @@ pub struct AllFacts<R: Atom, L: Atom, P: Atom, V: Atom, M: Atom> {
     /// `killed(B,P)` when some prefix of the path borrowed at B is assigned at point P
     pub killed: Vec<(L, P)>,
 
-    /// `outlives(R1, R2, P)` when we require `R1@P: R2@P`
-    pub outlives: Vec<(R, R, P)>,
+    /// `outlives(O1, P2, P)` when we require `O1@P: O2@P`
+    pub outlives: Vec<(Origin, Origin, P)>,
 
     ///  `invalidates(P, L)` when the loan L is invalidated at point P
     pub invalidates: Vec<(P, L)>,
@@ -33,12 +33,12 @@ pub struct AllFacts<R: Atom, L: Atom, P: Atom, V: Atom, M: Atom> {
     /// `var_used(V, P) when the variable V is used in a drop at point P`
     pub var_drop_used: Vec<(V, P)>,
 
-    /// `var_uses_region(V, R) when the type of V includes the region R`
-    pub var_uses_region: Vec<(V, R)>,
+    /// `var_uses_region(V, O)` when the type of V includes the origin `O`
+    pub var_uses_region: Vec<(V, Origin)>,
 
-    /// `var_drops_region(V, R) when the type of V includes the region R and uses
-    /// it when dropping`
-    pub var_drops_region: Vec<(V, R)>,
+    /// `var_drops_region(V, O)` when the type of V includes the origin `O` and uses
+    /// it when dropping
+    pub var_drops_region: Vec<(V, Origin)>,
 
     /// `child(M1, M2) when the move path `M1` is the direct or transitive child
     /// of `M2`, e.g. `child(x.y, x)`, `child(x.y.z, x.y)`, `child(x.y.z, x)`
@@ -64,7 +64,7 @@ pub struct AllFacts<R: Atom, L: Atom, P: Atom, V: Atom, M: Atom> {
     pub path_accessed_at: Vec<(M, P)>,
 }
 
-impl<R: Atom, L: Atom, P: Atom, V: Atom, M: Atom> Default for AllFacts<R, L, P, V, M> {
+impl<Origin: Atom, L: Atom, P: Atom, V: Atom, M: Atom> Default for AllFacts<Origin, L, P, V, M> {
     fn default() -> Self {
         AllFacts {
             borrow_region: Vec::default(),
