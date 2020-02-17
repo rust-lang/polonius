ed924dbc90a2a73dd140da5d7adfef39ca74ffa6
Albin Stjerna <albin.stjerna@gmail.com>
2020-02-17 19:16:59 +0100

    Update collect-facts.sh to only collect the needed facts

--- inputs/collect-facts.sh
+++ inputs/collect-facts.sh
@@ -2,11 +2,7 @@
 
 RUSTC_RELEASE="stage1"
 RUSTC_ARGS="-Znll-facts -Zborrowck=mir"
-INPUT_FOLDERS=(drop-liveness drop-may-dangle drop-no-may-dangle enum-drop-access
-               issue-47680 issue-52059-report-when-borrow-and-drop-conflict
-               maybe-initialized-drop maybe-initialized-drop-implicit-fragment-drop
-               maybe-initialized-drop-uninitialized maybe-initialized-drop-with-fragment
-               maybe-initialized-drop-with-uninitialized-fragments smoke-test vec-push-ref)
+INPUT_FOLDERS=(issue-47680 smoke-test vec-push-ref)
 
 for test_folder in "${INPUT_FOLDERS[@]}";
 do
