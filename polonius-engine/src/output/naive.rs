a998edd684dc6df1e92f29b07e7b9408b82fe1ea
Rémy Rakic <remy.rakic+github@gmail.com>
2020-12-21 16:09:03 +0100

    naive: number and document the rules
    This will help readability, and documentation, when we extract the rules to the book. Also describes more precisely why we need a duplicate liveness relation.

--- polonius-engine/src/output/naive.rs
+++ polonius-engine/src/output/naive.rs
@@ -45,6 +45,11 @@ pub(super) fn compute<T: FactTypes>(
         // `loan_invalidated_at` facts, stored ready for joins
         let loan_invalidated_at =
             iteration.variable::<((T::Loan, T::Point), ())>("loan_invalidated_at");
+        loan_invalidated_at.extend(
+            ctx.loan_invalidated_at
+                .iter()
+                .map(|&(loan, point)| ((loan, point), ())),
+        );
 
         // different indices for `subset`.
         let subset_o1p = iteration.variable_indistinct("subset_o1p");
@@ -54,10 +59,32 @@ pub(super) fn compute<T: FactTypes>(
         let origin_contains_loan_on_entry_op =
             iteration.variable_indistinct("origin_contains_loan_on_entry_op");
 
-        // we need `origin_live_on_entry` in both variable and relation forms.
-        // (respectively, for the regular join and the leapjoin).
+        // Unfortunately, we need `origin_live_on_entry` in both variable and relation forms:
+        // We need:
+        // - `origin_live_on_entry` as a Relation for the leapjoins in rules 3 & 6
+        // - `origin_live_on_entry` as a Variable for the join in rule 7
+        //
+        // The leapjoins use `origin_live_on_entry` as `(Origin, Point)` tuples, while the join uses
+        // it as a `((O, P), ())` tuple to filter the `((Origin, Point), Loan)` tuples from
+        // `origin_contains_loan_on_entry_op`.
+        //
+        // The regular join in rule 7 could be turned into a `filter_with` leaper but that would
+        // result in a leapjoin with no `extend_*` leapers: a leapjoin that is not well-formed.
+        // Doing the filtering via an `extend_with` leaper would be extremely inefficient.
+        //
+        // Until there's an API in datafrog to handle this use-case better, we do a slightly less
+        // inefficient thing of copying the whole static input into a Variable to use a regular
+        // join, even though the liveness information can be quite heavy (around 1M tuples
+        // on `clap`).
+        // This is the Naive variant so this is not a big problem, but needs an
+        // explanation.
         let origin_live_on_entry_var =
             iteration.variable::<((T::Origin, T::Point), ())>("origin_live_on_entry");
+        origin_live_on_entry_var.extend(
+            origin_live_on_entry_rel
+                .iter()
+                .map(|&(origin, point)| ((origin, point), ())),
+        );
 
         // variable and index to compute the subsets of placeholders
         let subset_placeholder =
@@ -68,19 +95,19 @@ pub(super) fn compute<T: FactTypes>(
         let errors = iteration.variable("errors");
         let subset_errors = iteration.variable::<(T::Origin, T::Origin, T::Point)>("subset_errors");
 
-        // load initial facts.
+        // load initial facts:
+
+        // Rule 1: the initial subsets are the non-transitive `subset_base` static input.
+        //
+        // subset(Origin1, Origin2, Point) :-
+        //   subset_base(Origin1, Origin2, Point).
         subset.extend(ctx.subset_base.iter());
+
+        // Rule 4: the issuing origins are the ones initially containing loans.
+        //
+        // origin_contains_loan_on_entry(Origin, Loan, Point) :-
+        //   loan_issued_at(Origin, Loan, Point).
         origin_contains_loan_on_entry.extend(ctx.loan_issued_at.iter());
-        loan_invalidated_at.extend(
-            ctx.loan_invalidated_at
-                .iter()
-                .map(|&(loan, point)| ((loan, point), ())),
-        );
-        origin_live_on_entry_var.extend(
-            origin_live_on_entry_rel
-                .iter()
-                .map(|&(origin, point)| ((origin, point), ())),
-        );
 
         // .. and then start iterating rules!
         while iteration.changed() {
@@ -104,7 +131,7 @@ pub(super) fn compute<T: FactTypes>(
                 .elements
                 .retain(|&(origin1, origin2, _)| origin1 != origin2);
 
-            // remap fields to re-index by keys.
+            // Remap fields to re-index by keys, to prepare the data needed by the rules below.
             subset_o1p.from_map(&subset, |&(origin1, origin2, point)| {
                 ((origin1, point), origin2)
             });
@@ -121,24 +148,26 @@ pub(super) fn compute<T: FactTypes>(
                     ((origin, point), loan)
                 });
 
-            // subset(origin1, origin2, point) :-
-            //   subset_base(origin1, origin2, point).
-            // Already loaded; `subset_base` is static.
+            // Rule 1: done above, as part of the static input facts setup.
 
-            // subset(origin1, origin3, point) :-
-            //   subset(origin1, origin2, point),
-            //   subset(origin2, origin3, point).
+            // Rule 2: compute the subset transitive closure, at a given point.
+            //
+            // subset(Origin1, Origin3, Point) :-
+            //   subset(Origin1, Origin2, Point),
+            //   subset(Origin2, Origin3, Point).
             subset.from_join(
                 &subset_o2p,
                 &subset_o1p,
                 |&(_origin2, point), &origin1, &origin3| (origin1, origin3, point),
             );
 
-            // subset(origin1, origin2, point2) :-
-            //   subset(origin1, origin2, point1),
-            //   cfg_edge(point1, point2),
-            //   origin_live_on_entry(origin1, point2),
-            //   origin_live_on_entry(origin2, point2).
+            // Rule 3: propagate subsets along the CFG, according to liveness.
+            //
+            // subset(Origin1, Origin2, Point2) :-
+            //   subset(Origin1, Origin2, Point1),
+            //   cfg_edge(Point1, Point2),
+            //   origin_live_on_entry(Origin1, Point2),
+            //   origin_live_on_entry(Origin2, Point2).
             subset.from_leapjoin(
                 &subset,
                 (
@@ -149,24 +178,26 @@ pub(super) fn compute<T: FactTypes>(
                 |&(origin1, origin2, _point1), &point2| (origin1, origin2, point2),
             );
 
-            // origin_contains_loan_on_entry(origin, loan, point) :-
-            //   loan_issued_at(origin, loan, point).
-            // Already loaded; `loan_issued_at` is static.
+            // Rule 4: done above as part of the static input facts setup.
 
-            // origin_contains_loan_on_entry(origin2, loan, point) :-
-            //   origin_contains_loan_on_entry(origin1, loan, point),
-            //   subset(origin1, origin2, point).
+            // Rule 5: propagate loans within origins, at a given point, according to subsets.
+            //
+            // origin_contains_loan_on_entry(Origin2, Loan, Point) :-
+            //   origin_contains_loan_on_entry(Origin1, Loan, Point),
+            //   subset(Origin1, Origin2, Point).
             origin_contains_loan_on_entry.from_join(
                 &origin_contains_loan_on_entry_op,
                 &subset_o1p,
                 |&(_origin1, point), &loan, &origin2| (origin2, loan, point),
             );
 
-            // origin_contains_loan_on_entry(origin, loan, point2) :-
-            //   origin_contains_loan_on_entry(origin, loan, point1),
-            //   !loan_killed_at(loan, point1),
-            //   cfg_edge(point1, point2),
-            //   origin_live_on_entry(origin, point2).
+            // Rule 6: propagate loans along the CFG, according to liveness.
+            //
+            // origin_contains_loan_on_entry(Origin, Loan, Point2) :-
+            //   origin_contains_loan_on_entry(Origin, Loan, Point1),
+            //   !loan_killed_at(Loan, Point1),
+            //   cfg_edge(Point1, Point2),
+            //   origin_live_on_entry(Origin, Point2).
             origin_contains_loan_on_entry.from_leapjoin(
                 &origin_contains_loan_on_entry,
                 (
@@ -177,24 +208,31 @@ pub(super) fn compute<T: FactTypes>(
                 |&(origin, loan, _point1), &point2| (origin, loan, point2),
             );
 
-            // loan_live_at(loan, point) :-
-            //   origin_contains_loan_on_entry(origin, loan, point),
-            //   origin_live_on_entry(origin, point).
+            // Rule 7: compute whether a loan is live at a given point, i.e. whether it is
+            // contained in a live origin at this point.
+            //
+            // loan_live_at(Loan, Point) :-
+            //   origin_contains_loan_on_entry(Origin, Loan, Point),
+            //   origin_live_on_entry(Origin, Point).
             loan_live_at.from_join(
                 &origin_contains_loan_on_entry_op,
                 &origin_live_on_entry_var,
                 |&(_origin, point), &loan, _| ((loan, point), ()),
             );
 
-            // errors(loan, point) :-
-            //   loan_invalidated_at(loan, point),
-            //   loan_live_at(loan, point).
+            // Rule 8: compute illegal access errors, i.e. an invalidation of a live loan.
+            //
+            // errors(Loan, Point) :-
+            //   loan_invalidated_at(Loan, Point),
+            //   loan_live_at(Loan, Point).
             errors.from_join(
                 &loan_invalidated_at,
                 &loan_live_at,
                 |&(loan, point), _, _| (loan, point),
             );
 
+            // Rule 9: compute the subsets of the placeholder origins, at a given point.
+            //
             // subset_placeholder(Origin1, Origin2, Point) :-
             //   subset(Origin1, Origin2, Point),
             //   placeholder_origin(Origin1).
@@ -210,6 +248,8 @@ pub(super) fn compute<T: FactTypes>(
                 |&((origin1, point), origin2), _| (origin1, origin2, point),
             );
 
+            // Rule 10: compute the subset transitive closure of placeholder origins.
+            //
             // subset_placeholder(Origin1, Origin3, Point) :-
             //   subset_placeholder(Origin1, Origin2, Point),
             //   subset(Origin2, Origin3, Point).
@@ -219,6 +259,9 @@ pub(super) fn compute<T: FactTypes>(
                 |&(_origin2, point), &origin1, &origin3| (origin1, origin3, point),
             );
 
+            // Rule 11: compute illegal subset relations errors, i.e. the undeclared subsets
+            // between two placeholder origins.
+            //
             // subset_errors(Origin1, Origin2, Point) :-
             //   subset_placeholder(Origin1, Origin2, Point),
             //   placeholder_origin(Origin2),
